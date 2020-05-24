161bc6224a37b01a97de4b2396a9a0e332d0d5ec
Rick Herrick <jrherrick@wustl.edu>
2020-05-24 14:42:16 -0500

    Modified XNAT and PostgreSQL UID and GID settings, trying to get it to work with or without values set

--- xnat/wait-for-postgres.sh
+++ xnat/wait-for-postgres.sh
@@ -5,6 +5,11 @@ set -e
 
 cmd="$@"
 
+# If xnat user exists, use that to run catalina.sh
+if [ $(id -u xnat) == 0 ]; then
+    cmd="su xnat ${cmd}"
+fi
+
 until psql -U "$XNAT_DATASOURCE_USERNAME" -h xnat-db -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 5
@@ -12,3 +17,4 @@ done
 
 >&2 echo "Postgres is up - executing command \"$cmd\""
 exec $cmd
+
