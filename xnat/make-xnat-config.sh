8045f3dd86038964e3c232f3e298e131ab379d34
Rick Herrick <jrherrick@wustl.edu>
2020-08-04 09:31:32 -0500

    Tweaked pipeline download and configuration to get set up properly

--- xnat/make-xnat-config.sh
+++ xnat/make-xnat-config.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # generate xnat config
-if [ ! -f $XNAT_HOME/config/xnat-conf.properties ]; then
-  cat > $XNAT_HOME/config/xnat-conf.properties << EOF
-datasource.driver=$XNAT_DATASOURCE_DRIVER
-datasource.url=$XNAT_DATASOURCE_URL
-datasource.username=$XNAT_DATASOURCE_USERNAME
-datasource.password=$XNAT_DATASOURCE_PASSWORD
+if [ ! -f ${XNAT_HOME}/config/xnat-conf.properties ]; then
+  cat > ${XNAT_HOME}/config/xnat-conf.properties << EOF
+datasource.driver=${XNAT_DATASOURCE_DRIVER}
+datasource.url=${XNAT_DATASOURCE_URL}
+datasource.username=${XNAT_DATASOURCE_USERNAME}
+datasource.password=${XNAT_DATASOURCE_PASSWORD}
 
-hibernate.dialect=$XNAT_HIBERNATE_DIALECT
+hibernate.dialect=${XNAT_HIBERNATE_DIALECT}
 hibernate.hbm2ddl.auto=update
 hibernate.show_sql=false
 hibernate.cache.use_second_level_cache=true
 hibernate.cache.use_query_cache=true
 
-spring.activemq.broker-url=$XNAT_ACTIVEMQ_URL
-spring.activemq.user=$XNAT_ACTIVEMQ_USERNAME
-spring.activemq.password=$XNAT_ACTIVEMQ_PASSWORD
+spring.activemq.broker-url=${XNAT_ACTIVEMQ_URL}
+spring.activemq.user=${XNAT_ACTIVEMQ_USERNAME}
+spring.activemq.password=${XNAT_ACTIVEMQ_PASSWORD}
 
 spring.http.multipart.max-file-size=1073741824
 spring.http.multipart.max-request-size=1073741824
@@ -24,16 +24,16 @@ EOF
 fi
 
 mkdir -p /usr/local/share/xnat
-cp $XNAT_HOME/config/xnat-conf.properties /usr/local/share/xnat
+cp ${XNAT_HOME}/config/xnat-conf.properties /usr/local/share/xnat
 
 [[ "${INSTALL_PIPELINE}" == "false" ]] && { echo "Skipping pipeline installation"; exit 0; }
 
-wget --quiet https://ci.xnat.org/job/pipeline/job/xnat-pipeline-engine/lastSuccessfulReleaseBuild/artifact/build/libs/xnat-pipeline-$XNAT_VER.zip
-unzip -qq xnat-pipeline-$XNAT_VER.zip
+wget --quiet --output-file=${XNAT_PIPELINE_ARCHIVE} ${XNAT_PIPELINE_URL}
+unzip -qq ${XNAT_PIPELINE_ARCHIVE}
 cd xnat-pipeline
 
 cat > gradle.properties << GRADLE_PROPS
-xnatUrl=http://localhost
+xnatUrl=${XNAT_PROCESSING_URL}
 siteName=XNAT
 adminEmail=${XNAT_EMAIL}
 smtpServer=${SMTP_HOSTNAME}
@@ -42,5 +42,5 @@ GRADLE_PROPS
 
 ./gradlew
 cd ..
-rm -rf xnat-pipeline-$XNAT_VER.zip xnat-pipeline
+rm -rf ${XNAT_PIPELINE_ARCHIVE} xnat-pipeline
 
