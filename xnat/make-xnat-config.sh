c75904c27d50897dc8987077a4e296053efcda73
Matt Kelsey <kelseym@mir.wustl.edu>
2021-03-04 17:59:59 -0600

    Updated Postgres to 12.2

--- xnat/make-xnat-config.sh
+++ xnat/make-xnat-config.sh
@@ -7,11 +7,36 @@ datasource.driver=$XNAT_DATASOURCE_DRIVER
 datasource.url=$XNAT_DATASOURCE_URL
 datasource.username=$XNAT_DATASOURCE_USERNAME
 datasource.password=$XNAT_DATASOURCE_PASSWORD
-hibernate.dialect=$XNAT_HIBERNATE_DIALECT
+hibernate.dialect=org.hibernate.dialect.PostgreSQL9Dialect
 hibernate.hbm2ddl.auto=update
 hibernate.show_sql=false
 hibernate.cache.use_second_level_cache=true
 hibernate.cache.use_query_cache=true
+
+spring.http.multipart.max-file-size=1073741824
+spring.http.multipart.max-request-size=1073741824
 EOF
 fi
+
+
+if [ ! -z "${XNAT_EMAIL}" ]; then
+  cat > ${XNAT_HOME}/config/prefs-init.ini << EOF
+[siteConfig]
+adminEmail=${XNAT_EMAIL}
+EOF
+fi
+
+if [ "${XNAT_SMTP_ENABLED}" == "true" ]; then
+  cat >> ${XNAT_HOME}/config/prefs-init.ini << EOF
+[notifications]
+smtpEnabled=true
+smtpHostname=${XNAT_SMTP_HOSTNAME}
+smtpPort=${XNAT_SMTP_PORT}
+smtpUsername=${XNAT_SMTP_USERNAME}
+smtpPassword=${XNAT_SMTP_PASSWORD}
+smtpAuth=${XNAT_SMTP_AUTH}
+EOF
+fi
+
+
 
